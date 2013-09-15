dd5c59b880f41664357160c44f80db5248d128bd
David Virgilio <drvirgilio@gmail.com>
2013-09-15 18:51:39 -0500

    buffered command now sends \n until the device echos \n or it times out

--- gsioc.py
+++ gsioc.py
@@ -31,14 +31,14 @@ class gsioc:
 
     def closeSerial(self):
         self.serial.close()
-    
+
     def connect(self,ID=0):
         if( int(ID) not in range(64) ):
             raise Exception("ID out of range [0,63]")
         ID += 128
         s = self.serial
         s.flushInput()
-        s.write(bytes.fromhex('ff'))   
+        s.write(bytes.fromhex('ff'))
         time.sleep(self.timeout)   # Passively wait for all devices to disconnect
         s.write(ID.to_bytes(1,byteorder='big'))
         resp = s.read(1)    # Will raise serialTimoutException after timeout
@@ -63,17 +63,26 @@ class gsioc:
             else:
                 s.write(bytes.fromhex("06"))
         return resp.decode("ascii")
-        
+
     def bCommand(self,commandstring):
         data = binascii.a2b_qp("\n" + commandstring + "\r")
         s = self.serial
         s.flushInput()
-        s.write(data[0:1])    # send line feed
-        resp = bytearray(0)
-        resp.append(s.read(1)[0])
-        if(resp[0] != 10):
-            print(resp)
-            raise Exception("Did not recieve \\n (0x0A) as response")
+
+        # begin buffered command by sending \n until the device echos \n or times out
+        # start time
+        startTime = time.time()
+        # begin loop
+        while(true):
+            s.write(data[0:1])    # send line feed
+            readySig = s.read(1)[0]
+            if(readySig == 10) break
+            if(time.time()-startTime > self.timeout):
+                print(readySig)
+                raise Exception("Did not recieve \\n (0x0A) as response")
+        resp.append(readySig)
+
+        # Send buffered data
         for i in range(1,len(data)):
             s.write(data[i:i+1])
             resp.append(s.read(1)[0])
@@ -82,6 +91,8 @@ class gsioc:
             if( resp[i] == 13 ):
                 print(str(datetime.datetime.now()) + " -- Buffered command complete")
                 return resp
+
+        # This will happen if sending the data failed
         print(str(datetime.datetime.now()) + " -- Buffered command FAILED")
         resp_no_whitespace = resp[1:len(resp)-2]
         return resp_no_whitespace.decode("ascii")
