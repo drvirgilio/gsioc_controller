df4af306e317ce1f4c0d8db074d46c5e7b0d93ee
David Virgilio <echo-oddly@users.noreply.github.com>
2017-12-14 00:05:14 -0600

    Raise exception if there is no response from device. See #2

--- gsioc.py
+++ gsioc.py
@@ -41,7 +41,9 @@ class gsioc:
         s.write(bytes.fromhex('ff'))
         time.sleep(self.timeout)   # Passively wait for all devices to disconnect
         s.write(ID.to_bytes(1,byteorder='big'))
-        resp = s.read(1)    # Will raise serialTimoutException after timeout
+        resp = s.read(1)    # Will return empty array after timeout
+        if(len(resp) == 0):
+            raise Exception(str(datetime.datetime.now()) + "No response from device")
         print(str(datetime.datetime.now()) + " -- Connected to device ", ID-128)
 
     # returns byte array
@@ -55,7 +57,10 @@ class gsioc:
         s.write(command[0:1])
         resp = bytearray(0)
         while(True):
-            resp.append(s.read(1)[0])
+            resp_raw = s.read(1)    # Will return empty array after timeout
+            if(len(resp_raw) == 0):
+                raise Exception(str(datetime.datetime.now()) + "No response from device")
+            resp.append(resp_raw[0])
             if(resp[len(resp)-1] > 127):
                 resp[len(resp)-1] -= 128
                 print(str(datetime.datetime.now()) + " -- Immediate response complete")
@@ -75,7 +80,10 @@ class gsioc:
         # begin loop
         while(True):
             s.write(data[0:1])    # send line feed
-            readySig = s.read(1)[0]
+            resp_raw = s.read(1)    # Will return empty array after timeout
+            if(len(resp_raw) == 0):
+                raise Exception(str(datetime.datetime.now()) + "No response from device")
+            readySig = resp_raw[0]
             if(readySig == 10):
                 print(str(datetime.datetime.now()) + " -- Starting Buffered command")
                 break
@@ -90,7 +98,10 @@ class gsioc:
         # Send buffered data
         for i in range(1,len(data)):
             s.write(data[i:i+1])
-            resp.append(s.read(1)[0])
+            resp_raw = s.read(1)    # Will return empty array after timeout
+            if(len(resp_raw) == 0):
+                raise Exception(str(datetime.datetime.now()) + "No response from device")
+            resp.append(resp_raw[0])
             if( resp[i] != data[i] ):
                 raise Exception("Recieved " + str(resp,'ascii') + " instead of " + str(data[i:i+1]))
             if( resp[i] == 13 ):
